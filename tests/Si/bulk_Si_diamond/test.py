83740488d04510d6ba46f5c80c8082e2736cfda8
MorrowChem <joemorrow117@gmail.com>
2022-09-22 10:39:54 +0100

    clear git cache
    Former-commit-id: 7a84a3454aa9f77da48181abaf3960cb7eb2e311

--- tests/Si/bulk_Si_diamond/test.py
+++
@@ -1,4 +0,0 @@
-import os.path, lattice
-from utilities import relax_config
-
-properties = lattice.do_lattice(os.path.abspath(os.path.dirname(__file__)), 'cubic')
--- tests/Si/point_defect_Si_diamond_interstitial_hex/test.py
+++
@@ -1,3 +0,0 @@
-import os.path, interstitial
-
-properties = interstitial.do_all_interstitials(os.path.abspath(os.path.dirname(__file__)), nn_cutoff=2.7)
