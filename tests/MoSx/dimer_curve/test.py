54f79ce10d2dcfb27dfb1695ccdeb5c72fd3e2e7
MorrowChem <joemorrow117@gmail.com>
2023-03-29 09:46:35 +0100

    added VASP models and dimer_curve test ready to put on ARCHER2

--- tests/MoSx/dimer_curve/test.py
+++ tests/MoSx/dimer_curve/test.py
@@ -1,13 +1,21 @@
 import model
 from ase.io import read, write
 from ase import Atoms
+from ase.calculators.vasp import Vasp
 import numpy as np
+import os
+from copy import deepcopy
+
 
 directory = os.getcwd()
 i=0
 model.calculator.directory = directory + '/s{:04d}'.format(i)
-
 mi = 1.0; ma = 5.5; n=50
+
+if isinstance(model.calculator, Vasp):
+    model.calculator.set(kspacing=100, nbands=24) # no need for kpoints, use exact diagonalization
+    n = 15 # do fewer steps for VASP
+
 
 ########################################
 
@@ -31,14 +39,16 @@ traj = [[] for at in ats]
 es = [[] for at in ats]
 
 x = np.linspace(mi, ma, n)
-for i in x:
+for ct, i in enumerate(x):
     pos[1, 2] = i
     ats_copy = [at.copy() for at in ats]
-    for i in range(len(ats)):
-        ats[i].set_positions(pos)
-        traj[i].append(ats_copy[i])
-        es[i].append(ats[i].get_potential_energy())
-        write('./dimer_traj_f{names[i]}.xyz', traj[i])
+    ats_copy.calc.set(directory= ats_copy.calc._directory + f'_0000')
+    for a in range(len(ats)):
+        ats_copy.set_positions(pos)
+        traj[a].append(ats_copy[a])
+        ats_copy.calc.set(directory= ats_copy.calc._directory[:-4] + f'{a:04d}')
+        es[a].append(ats_copy.get_potential_energy())
+        write('./dimer_traj_f{names[i]}.xyz', traj[a])
 
 es = np.array(es) - np.array(e0s)[:, np.newaxis]
 
