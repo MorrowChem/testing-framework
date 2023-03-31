bde9edbcd1e1ca91f1ded723f418ef0b7fc62ac2
MorrowChem <joemorrow117@gmail.com>
2023-03-31 15:39:05 +0100

    finished dimer curves test

--- tests/MoSx/dimer_curve/test.py
+++ tests/MoSx/dimer_curve/test.py
@@ -14,7 +14,7 @@ mi = 1.0; ma = 5.5; n=50
 
 if isinstance(model.calculator, Vasp):
     model.calculator.set(kspacing=100, nbands=24) # no need for kpoints, use exact diagonalization
-    n = 15 # do fewer steps for VASP
+    n = 30 # do fewer steps for VASP
 
 
 ########################################
@@ -32,6 +32,7 @@ for i in range(len(ats)):
     calc = deepcopy(model.calculator)
     calc.set(directory=model.calculator.directory[:-4] + '{:04d}'.format(i+1))
     ats[i].calc = calc
+    print('Setting original calc directory ', ats[i].calc._directory)
 
 e0s = [at.get_potential_energy() for at in ats]
 
@@ -39,25 +40,27 @@ traj = [[] for at in ats]
 es = [[] for at in ats]
 
 x = np.linspace(mi, ma, n)
-for ct, i in enumerate(x):
-    pos[1, 2] = i
-    ats_copy = [at.copy() for at in ats]
-    ats_copy.calc.set(directory= ats_copy.calc._directory + f'_0000')
-    for a in range(len(ats)):
-        ats_copy.set_positions(pos)
+print('Separations ', x[0], ' to ', x[-1])
+
+ats_copy = [at.copy() for at in ats]
+for at_ct, at in enumerate(ats_copy):
+    ats_copy[at_ct].calc = deepcopy(ats[at_ct].calc)
+    ats_copy[at_ct].calc.set(directory= ats[at_ct].calc._directory + f'_0000')
+for a in range(len(ats)):
+    for ct, i in enumerate(x):
+        pos[1, 2] = i
+        ats_copy[a].set_positions(pos)
         traj[a].append(ats_copy[a])
-        ats_copy.calc.set(directory= ats_copy.calc._directory[:-4] + f'{a:04d}')
-        es[a].append(ats_copy.get_potential_energy())
-        write('./dimer_traj_f{names[i]}.xyz', traj[a])
+        ats_copy[a].calc.set(directory=ats_copy[a].calc._directory[:-4] + f'{ct:04d}')
+        print('setting ats_copy directory to ', ats_copy[a].calc._directory)
+        es[a].append(ats_copy[a].get_potential_energy())
+        write(f'./dimer_traj_f{names[i]}.xyz', traj[a])
 
 es = np.array(es) - np.array(e0s)[:, np.newaxis]
 
-print('Separations ', x)
 print("Energies ")
 for i in range(len(ats)):
     print(names[i], ' ', es[i])
-# from scipy import interpolate
-# spline = interpolate.splrep(x, es, s=0)
 
 properties = {'dimer_separations': x.tolist()}
 properties.update({f'{names[i]}_dimer_energies': es[i].tolist() for i in range(len(ats))})
